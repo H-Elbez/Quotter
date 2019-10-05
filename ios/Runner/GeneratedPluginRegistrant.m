5c6932760340fdf942ca2e868d2710149c72b15e
Elbez Hammouda <hammoudae@gmail.com>
2019-10-05 19:09:09 +0100

    Code updated

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <clipboard_manager/ClipboardManagerPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ClipboardManagerPlugin registerWithRegistrar:[registry registrarForPlugin:@"ClipboardManagerPlugin"]];
+}
+
+@end
---
+++ lib/main.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:clipboard_manager/clipboard_manager.dart';
+import 'package:toast/toast.dart';
+
+void main() {
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quotter',
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<MainPage> {
+  String quote, owner, imglink;
+  bool working = false;
+  final grey = Colors.blueGrey[800];
+  @override
+  void initState() {
+    super.initState();
+    quote = "";
+    owner = "";
+    imglink = "";
+    getQuote();
+  }
+
+  getQuote() async {
+    try {
+      setState(() {
+        working = true;
+        quote = imglink = owner = "";
+      });
+      var response = await http.post(
+          Uri.encodeFull('http://api.forismatic.com/api/1.0/'),
+          body: {"method": "getQuote", "format": "json", "lang": "en"});
+      setState(() {
+        try {
+          var res = jsonDecode(response.body);
+          owner = res["quoteAuthor"].toString().trim();
+          quote = res["quoteText"].replaceAll("â", " ");
+          getImg(owner);
+        } catch (e) {
+          getQuote();
+        }
+      });
+    } catch (e) {
+      offline();
+    }
+  }
+
+  offline() {
+    setState(() {
+      owner = "Janet Fitch";
+      quote = "The phoenix must burn to emerge";
+      imglink = "";
+      working = false;
+    });
+  }
+
+  copyQuote() {
+    ClipboardManager.copyToClipBoard(quote + "\n- " + owner).then((result) {
+      Toast.show("Quote Copied", context, duration: Toast.LENGTH_SHORT);
+    });
+  }
+
+  getImg(String name) async {
+    var image = await http.get(
+        "https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrlimit=1&prop=pageimages%7Cextracts&pithumbsize=400&gsrsearch=" +
+            name +
+            "&format=json");
+
+    setState(() {
+      try {
+        var res = json.decode(image.body)["query"]["pages"];
+        res = res[res.keys.first];
+        imglink = res["thumbnail"]["source"];
+      } catch (e) {
+        imglink = "";
+      }
+      working = false;
+    });
+  }
+
+  Widget drawImg() {
+    if (imglink.isEmpty) {
+      return Image.asset("img/offline.jpg", fit: BoxFit.cover);
+    } else {
+      return Image.network(imglink, fit: BoxFit.cover);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: grey,
+      body: Stack(
+          alignment: Alignment.center,
+          fit: StackFit.expand,
+          children: <Widget>[
+            drawImg(),
+            Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0, 0.6, 1],
+                    colors: [
+                      grey.withAlpha(70),
+                      grey.withAlpha(220),
+                      grey.withAlpha(255),
+                    ],
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 100),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          text: quote != null ?'“ ':"",
+                          style: TextStyle(
+                              fontFamily: "Ic",
+                              color: Colors.green,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 30),
+                          children: [
+                            TextSpan(text: 
+                            quote != null ? quote : "",
+                          style: TextStyle(
+                              fontFamily: "Ic",
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 22)
+                            ),
+                            TextSpan(text: quote != null ?'”':"",style: TextStyle(
+                              fontFamily: "Ic",
+                              fontWeight: FontWeight.w700,
+                              color: Colors.green,
+                              fontSize: 30))
+                          ]
+                        ),
+                      ),
+                      Text(owner.isEmpty ? "" : "\n" + owner,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontFamily: "Ic",
+                              color: Colors.white,
+                              fontSize: 18)),
+                    ])),
+            AppBar(
+              title: Text(
+                "Quotter",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
+              ),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: true,
+            ),
+          ]),
+      floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            InkWell(
+              onTap: !working ? getQuote : null,
+              child: Icon(Icons.refresh, size: 35, color: Colors.white),
+            ),
+            InkWell(
+              onTap: quote.isNotEmpty ? copyQuote : null,
+              child: Icon(Icons.content_copy, size: 30, color: Colors.white),
+            )
+          ]),
+    );
+  }
+}
